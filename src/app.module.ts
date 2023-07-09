452b0f157af630ba3ef50df6d79845905ddda1e7
Victor Hugo Maciel de Lima <victorh5.empresa@gmail.com>
2023-07-08 20:41:47 -0400

    creating user routes, findAll, findOne, create, update, remove

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client/dist'),
     }),
+    MongooseModule.forRoot(
+      'mongodb+srv://victorh5900:IkTouXFfrCEO4Mfu@demarcotest.czbfbih.mongodb.net/',
+    ),
+    UsersModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+  @Prop()
+  name: string;
+  @Prop()
+  email: string;
+  @Prop()
+  password: string;
+  @Prop()
+  type: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
