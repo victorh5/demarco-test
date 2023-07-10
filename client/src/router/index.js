90f838b93f9ca6e4a64fed322c5c1dbc803e550e
Victor Hugo Maciel de Lima <victorh5.empresa@gmail.com>
2023-07-10 15:07:36 -0400

    adding new routes and rules at beforeEach

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import SignUp from '../views/SignUp.vue'
+import Evaluate from '../views/evaluate/Evaluate.vue'
+import FormEvaluate from '../views/evaluate/FormEvaluate.vue'
+import AnswerEvaluate from '../views/evaluate/AnswerEvaluate.vue'
+import store from '@/store'
 
 Vue.use(VueRouter)
 
@@ -16,12 +20,49 @@ const routes = [
     name: 'signup',
     component: SignUp
   },
+  {
+    path: '/evaluate',
+    name: 'evaluate',
+    component: Evaluate,
+    meta: { logged: true }
+  },
+  {
+    path: '/form-evaluate',
+    name: 'register-evaluate',
+    component: FormEvaluate,
+    meta: { logged: true }
+  },
+  {
+    path: '/form-evaluate/:id',
+    name: 'edit-evaluate',
+    component: FormEvaluate,
+    meta: { logged: true }
+  },
+  {
+    path: '/answer-evaluate/:id',
+    name: 'answer-evaluate',
+    component: AnswerEvaluate,
+    meta: { logged: true }
+  }
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const isLogged = store.getters.userIsLogged
+  if (to.matched.some(record => record.meta.logged)) {
+    if (isLogged) {
+      next()
+    } else {
+      next({ name: 'home' })
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
