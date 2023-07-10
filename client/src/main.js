6af310add9fce12662cb640ba4eb815faf5cb600
Victor Hugo Maciel de Lima <victorh5.empresa@gmail.com>
2023-07-10 15:09:15 -0400

    adding new global config

--- client/src/main.js
+++ client/src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import '@/assets/main.css'
+import api from './plugins/axios'
+import VueSweetalert2 from 'vue-sweetalert2'
+import '@/assets/scss/_main.scss'
+import 'sweetalert2/dist/sweetalert2.min.css'
 
 Vue.config.productionTip = false
+Vue.prototype.$http = api
+Vue.use(VueSweetalert2)
 
 new Vue({
   router,
