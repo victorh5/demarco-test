69627aea41b7c588dce81ec96d8c7af65cf3be7e
Victor Hugo Maciel de Lima <victorh5.empresa@gmail.com>
2023-07-11 02:09:14 -0400

    updating baseURL axios

--- client/src/plugins/axios.js
+++ client/src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://demarco-test-8398698c1bf5.herokuapp.com/api',
 });
 
 export default api;
